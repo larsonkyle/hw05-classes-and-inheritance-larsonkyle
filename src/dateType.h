aaaa3a00b75f3528512695be450f57e4b508e17d
Kyle Larson <122947142+larsonkyle@users.noreply.github.com>
2023-05-01 03:09:17 -0700

    Created dateType Class Header

---
+++ src/dateType.h
@@ -0,0 +1,22 @@
+#ifndef DATETYPE_H_
+#define DATETYPE_H_
+
+class dateType{
+  public: 
+    int getMonth() const;
+    int getDay() const;
+    int getYear() const;
+    void setDate(int, int, int);
+    dateType(int m = 0, int d = 0, int y = 0)
+      {  dMonth = m;
+         dDay = d;
+         dYear = y;  }
+
+  private:
+    int dMonth;
+    int dDay;
+    int dYear;
+};
+
+
+#endif
